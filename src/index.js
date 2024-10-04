a2d337a134a32578092fa69abd11711607454c58
Thomas Moon <thomasmoon0322@gmail.com>
2024-10-04 13:55:21 -0700

    Add DOM functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import './style.css'
-import { TodoManager } from './modules/todo_manager.js';
+import { DOMManager } from './modules/dom_manager'
 
-const gym = TodoManager.createProject('GYM!');
-const coding = TodoManager.createProject('Coding');
-
-
-console.log(gym)
-
-TodoManager.addTask(gym, 'deadlift', 'yup', 'tmrw', 'high');
-
-console.log(TodoManager.allProjects);
-console.log(TodoManager.allTasks);
-
+DOMManager.init();
 
 
 
--- src/modules/dom_manager.js
+++ src/modules/dom_manager.js
@@ -1,15 +1,58 @@
 import { TodoManager } from "./todo_manager";
 
 export const DOMManager = (function () {
-    function init() {
 
+    let currentProject;
+    const projectDialog = document.querySelector('.project_dialog');
+    const taskDialog = document.querySelector('.task_dialog');
+
+    function init() {
+        const newProjectButton = document.querySelector('.project_btn')
+        const viewTasksButton = document.querySelector('.task_btn')
+        newProjectButton.addEventListener('click', displayProject)
+        viewTasksButton.addEventListener('click', displayAllTasks)
     }
 
     function displayProject() {
+        const projectDialog = document.querySelector('.project_dialog');
+        const projectSubmit = document.querySelector('#project_submit');
+        const projectCancel = document.querySelector('#project_cancel');
+        projectSubmit.addEventListener('click', submitProject, false);
+        projectCancel.addEventListener('click', closeProject, false);
+        projectDialog.showModal();
+    }
 
+    function closeProject(event) {
+        event.preventDefault();
+        projectDialog.close();
+    }
+
+    function submitProject(event) {
+        event.preventDefault();
+        const sideBar = document.querySelector('.sidebar');
+        const title = document.getElementById('project_form').elements['title'].value
+        const sideDiv = document.createElement('div');
+        const project = document.createElement('p');
+        project.textContent = title;
+        sideBar.appendChild(sideDiv);
+        sideDiv.appendChild(project);
+        projectDialog.close()
     }
 
     function displayAllTasks() {
+        const display = document.querySelector('.main_display');
+        const testDiv = document.createElement('div');
+        testDiv.style.height = '100px';
+        display.appendChild(testDiv);
+    }
+
+    function displayTasks(project) {
+
+    }
+
+    function newTask(project) {
         
     }
+
+    return { init, displayProject, displayAllTasks, displayTasks }
 })()
